43f01da0cb6879ce0029d279bba301209a150011
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2023-10-12 16:29:23 +0200

    add the vote option in the smart contract

--- contract.sol
+++ contract.sol
@@ -2,13 +2,40 @@
 pragma solidity >=0.4.16 <0.9.0;
 
 contract ContrattoVotazioni {
-    bytes32[] private candidati;
+    mapping(bytes32 => uint256) private candidates;
+    bytes32[] private candidateList;
+    mapping(address => bool) private hasVoted;
 
-    function getCandidates() public view returns (bytes32[] memory) {
-        return candidati;
-    }
-    
     function addCandidate(bytes32 nomeCognome) public {
-        candidati.push(nomeCognome);
+        candidates[nomeCognome] = 0;
+        candidateList.push(nomeCognome);
+    }
+
+    function addVote(bytes32 nomeCognome) public {
+        require(!hasVoted[msg.sender], "Address has already voted.");
+        candidates[nomeCognome]++;
+        hasVoted[msg.sender] = true;
+    }
+
+    function getVotes(bytes32 nomeCognome) public view returns (uint256) {
+        return candidates[nomeCognome];
+    }
+
+    function getAllCandidates()
+        public
+        view
+        returns (bytes32[] memory, uint256[] memory)
+    {
+        uint256 candidateCount = candidateList.length;
+        bytes32[] memory names = new bytes32[](candidateCount);
+        uint256[] memory votes = new uint256[](candidateCount);
+
+        for (uint256 i = 0; i < candidateCount; i++) {
+            bytes32 candidateName = candidateList[i];
+            names[i] = candidateName;
+            votes[i] = candidates[candidateName];
+        }
+
+        return (names, votes);
     }
 }
--- js/main.js
+++ js/main.js
@@ -4,14 +4,16 @@ var connected = false;
 async function show_candidates() {
     show_loading(true)
     if (connected) {
-        const cands = await contract.getCandidates();
+        const cands = await contract.getAllCandidates();
         show_loading(false)
         console.log(cands);
         var ul = document.getElementById("list");
-        for (let i = 0; i < cands.length; i++) {
+        for (let i = 0; i < cands[0].length; i++) {
             var li = document.createElement("li");
-            const val = ethers.utils.parseBytes32String(cands[i]);
-            li.innerHTML = "<input id='default-radio-1' type='radio' value='' name='default-radio' class='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'><label for='default-radio-1' class='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>" + val + "</label>";
+            const val = ethers.utils.parseBytes32String(cands[0][i]);
+            const votes = cands[1][i];
+            console.log(votes.toNumber())
+            li.innerHTML = "<input id='default-radio-1' type='radio' value='' name='default-radio' class='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'><label for='default-radio-1' class='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>" + val + " &nbsp;" + votes + "</label>";
             ul.appendChild(li);
         }
     } else {
@@ -56,8 +58,8 @@ async function connectWallet() {
     console.log(signer);
     document.getElementById('connectButton').style.display = 'none';
     connected = true;
-    const abi = '[{"inputs":[{"internalType":"bytes32","name":"nomeCognome","type":"bytes32"}],"name":"addCandidate","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getCandidates","outputs":[{"internalType":"bytes32[]","name":"","type":"bytes32[]"}],"stateMutability":"view","type":"function"}]'
-    const contractAddress = "0x3B533DEaaCd1C3568CB75EE668A5787174746950"
+    const abi = '[ { "inputs": [ { "internalType": "bytes32", "name": "nomeCognome", "type": "bytes32" } ], "name": "addCandidate", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "nomeCognome", "type": "bytes32" } ], "name": "addVote", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getAllCandidates", "outputs": [ { "internalType": "bytes32[]", "name": "", "type": "bytes32[]" }, { "internalType": "uint256[]", "name": "", "type": "uint256[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "nomeCognome", "type": "bytes32" } ], "name": "getVotes", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ]'
+    const contractAddress = "0xeCac41161fd5978A4a3cEA2F6B39dDB4b3F51C4c"
     const contract1 = new ethers.Contract(contractAddress, abi, signer);
     contract = contract1;
     show_candidates();
