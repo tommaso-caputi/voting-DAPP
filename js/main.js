6a257ce74d2487cf0490c50d2ef8ae613be5860e
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2023-10-12 17:04:50 +0200

    set the vote option with error handle

--- js/main.js
+++ js/main.js
@@ -4,27 +4,21 @@ var connected = false;
 async function show_candidates() {
     show_loading(true)
     if (connected) {
+        document.getElementById("list").innerHTML = "";
         const cands = await contract.getAllCandidates();
         show_loading(false)
         console.log(cands);
         var ul = document.getElementById("list");
         for (let i = 0; i < cands[0].length; i++) {
-            var li = document.createElement("li");
-            const val = ethers.utils.parseBytes32String(cands[0][i]);
+            const name = ethers.utils.parseBytes32String(cands[0][i]);
             const votes = cands[1][i];
-            console.log(votes.toNumber())
-            li.innerHTML = "<input id='default-radio-1' type='radio' value='' name='default-radio' class='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'><label for='default-radio-1' class='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>" + val + " &nbsp;" + votes + "</label>";
+            var li = document.createElement("li");
+            li.innerHTML = "<input id='default-radio-1' type='radio' value='" + name + "' name='default-radio' class='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'><label for='default-radio-1' class='ml-2 text-sm font-medium text-gray-900 dark:text-gray-300'>" + name + " &nbsp;" + votes + "</label>";
             ul.appendChild(li);
         }
     } else {
         alert("Connect wallet");
     }
-}
-
-function delay(milliseconds) {
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds);
-    });
 }
 
 async function add_candidate() {
@@ -40,7 +34,6 @@ async function add_candidate() {
         console.log(receip);
 
         alert("Added " + document.getElementById("name").value);
-        document.getElementById("list").innerHTML = "";
         show_candidates();
     } else {
         alert("Connect wallet");
@@ -48,7 +41,22 @@ async function add_candidate() {
 }
 
 async function vote_candidate() {
-    console.log("das");
+    const selectedCandidate = document.querySelector('input[name="default-radio"]:checked');
+    if (selectedCandidate) {
+        show_loading(true)
+        try {
+            const vote = await contract.addVote(ethers.utils.formatBytes32String(selectedCandidate.value));
+            console.log(vote)
+            const receip = await vote.wait(1);
+            console.log(receip)
+        } catch (error) {
+            alert(error.error.message.substring(20,error.error.message.length));
+        }
+        show_loading(false)
+        show_candidates();
+    } else {
+        alert("No candidate selected");
+    }
 }
 
 async function connectWallet() {
@@ -68,10 +76,8 @@ async function connectWallet() {
 
 function show_loading(bool) {
     if (bool) {
-        console.log("start loading")
         document.getElementById("loader").style.display = 'block';
     } else {
-        console.log("end loading")
         document.getElementById("loader").style.display = 'none';
     }
 }
